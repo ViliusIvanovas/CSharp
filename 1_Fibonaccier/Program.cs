40d897ae0994fd5ca1614155ecbe4990e62a957c
Vilius Ivanovas <ivanovasvilius@gmail.com>
2022-09-05 17:30:45 +0200

    No, I did not nearly delete all of the progress i made due to a readme
    I tried updating readme to a funny haha, got punished. Luckily i had some small backup, so nothing major lost, though still scared me a bit

---
+++ 1_Fibonaccier/Program.cs
@@ -0,0 +1,30 @@
+﻿//Fibonachi sounds like a pasta name, cool
+uint num1 = 1;
+uint num2 = 0;
+uint fibNum;
+
+List<string> fibonachi = new List<string>();
+fibonachi.Add("0: " + num1);
+
+uint sequence = 1;
+
+for(;;)
+{
+    if (sequence < 100000)
+    {
+    fibNum = num1 + num2;
+    fibonachi.Add(sequence + ": " + fibNum);
+    num2 = num1;
+    num1 = fibNum;
+    sequence++;
+    }
+    else
+        break;
+}
+Console.WriteLine("The 13th fibonacci degit is:");
+Console.WriteLine(fibonachi[13]);
+Console.ReadKey();
+
+Console.WriteLine("Fibonacci list from 0 to 100 thousand");
+fibonachi.ForEach(i=> Console.Write("{0}\n", i));
+Console.ReadKey();
