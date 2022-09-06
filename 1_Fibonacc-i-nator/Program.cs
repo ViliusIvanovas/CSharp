acbf14b2ba229001889ab3d22887d4b4c70ea1ca
Vilius Ivanovas <ivanovasvilius@gmail.com>
2022-09-06 13:04:45 +0200

    Fibonacc-i-nator is amazing

--- 1_Fibonacc-i-nator/Program.cs
+++ 1_Fibonacc-i-nator/Program.cs
@@ -1,22 +1,26 @@
 ﻿//Fibonachi sounds like a pasta name, cool
+//uint lets us go higher up
 uint num1 = 1;
 uint num2 = 0;
 uint fibNum;
 
+//We make a list
 List<string> fibonacci = new List<string>();
 fibonacci.Add("1: " + num1);
 
-uint sequence = 2;
+//Iterations
+int i = 2;
 
 for(;;)
 {
-    if (sequence < 100)
+    //Run upto iteration 40 of fibonacci
+    if (i < 41)
     {
     fibNum = num1 + num2;
-    fibonacci.Add(sequence + ": " + fibNum);
+    fibonacci.Add(i + ": " + fibNum);
     num2 = num1;
     num1 = fibNum;
-    sequence++;
+    i++;
     }
     else
         break;
@@ -25,7 +29,7 @@ Console.WriteLine("The 13th fibonacci degit is:");
 Console.WriteLine(fibonacci[12]);
 Console.ReadKey();
 
-Console.WriteLine("Fibonacci list from 0 to 100:");
+Console.WriteLine("Fibonacci list from 0 to 40:");
 Thread.Sleep(500);
 fibonacci.ForEach(i=> Console.Write("{0}\n", i));
 Console.ReadKey();
